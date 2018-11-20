20ea4d6ca325e7fe19e6a930a8b153a03cb98e4b
Tural Mustafayev <mustafayevt@users.noreply.github.com>
2018-11-20 23:31:53 +0400

    Add project files.

---
+++ DelegateHomework.19.11.18/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateHomework._19._11._18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
